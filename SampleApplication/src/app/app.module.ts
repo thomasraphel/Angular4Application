9d06c75a8c7087086c167da23b8d2b2788c47bf7
thomas raphel <thomasrafel@gmail.com>
2018-10-11 14:47:18 -0400

    Loading ... will be gone at runtime since angular is runtime modification Component is just a class decorated with @Component @Component takes metadata, it takes a few parameters like template, selector, styles etc we need to import component decorator from angular core appmodule is also just a class decoreated with @NgModule parameters are arrays of imports, declarations, providers, bootstrap etc when a component is made through cli, it automatically registers the component in its corresponding module,also imports the component in the module class so that it can be registered

--- SampleApplication/src/app/app.module.ts
+++ SampleApplication/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServersComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
