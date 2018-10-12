391072b46ce608d10123830c2d45897e8b54e9c8
thomas raphel <thomasrafel@gmail.com>
2018-10-12 15:14:41 -0400

    added all the new components using g cli commands ng g c foldername/componentname --spec false creates the comonent in a folder of choice and with no spec.ts file

--- SampleApplication/src/app/app.module.ts
+++ SampleApplication/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent,
-    ServerComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
