dcb00c3c2742bd948b5885654a9c1183254e0f17
thomas raphel <thomasrafel@gmail.com>
2018-10-13 13:31:47 -0400

    img src uses property binding,, or either we can use src itself

--- SampleApplication/src/app/recipes/recipe-list/recipe-list.component.ts
+++ SampleApplication/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('chicken biriyani','Boil the chicken','http://www.freedigitalphotos.net/images/previews/gourmet-cheese-burgers-100388230.jpg'),
+    new Recipe('mutton biriyani','chop the mutton pieces','https://i.ytimg.com/vi/HI2-u2zu8Ss/maxresdefault.jpg')
+  ];
 
   constructor() { }
 
